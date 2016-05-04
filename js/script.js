3d9cb39c4379667013377d4b8432340cb4bc832b
Eric <ericluna1992@gmail.com>
2016-05-04 01:17:35 -0500

    added mor operation functionality for muliplication, division and decimals

--- js/script.js
+++ js/script.js
@@ -1,64 +1,66 @@
 $(document).ready(function(){
-	var number=0;
-	var total=0;
-	var things=$('.stuff').text();
-	var stuff=[];
-
+    var testNumLength = function(number) {
+        if (number.length > 17) {
+            display.text(number.substr(number.length-18,17));
+            // if (number.length > 15) {
+            //     number = "";
+            //     display.text("Err");
+            // }
+        } 
+    };
 	$('.clear').click(function(){
-		$('.stuff').empty();
-	})
+		$('#display').empty();
+	});
+	
 	// Numbers
-	$('.zero').click(function(){
-		$('.stuff').append(0);
-		stuff.push(0);
-	})
-	$('.one').click(function(){
-		$('.stuff').append(1);
-	})
-	$('.two').click(function(){
-		$('.stuff').append(2);
-	})
-	$('.three').click(function(){
-		$('.stuff').append(3);
-	})
-	$('.four').click(function(){
-		$('.stuff').append(4);
-	})
-	$('.five').click(function(){
-		$('.stuff').append(5);
-	})
-	$('.six').click(function(){
-		$('.stuff').append(6);
-	})
-	$('.seven').click(function(){
-		$('.stuff').append(7);
-	})
-	$('.eight').click(function(){
-		$('.stuff').append(8);
-	})
-	$('.nine').click(function(){
-		$('.stuff').append(9);
-	})
-	// Operations
-	$('.plus').click(function(){
-		things=Number($('.stuff').text());
-	})
-	$('.subtract').click(function(){
-		$('.stuff').append("-");
-	})
-	$('.multiply').click(function(){
-		$('.stuff').append("*");
-	})
-	$('.dot').click(function(){
-		$('.stuff').append(".");
-	})
-	$('.equal').click(function(){
-		$('.stuff').empty();
-		$('.stuff').append(total);
-		alert(stuff);
-	})
-	$('.division').click(function(){
-		$('.stuff').append("/");
-	})
+	$("#zero").click(function(){
+		$('#display').text($('#display').text() + 0);
+    });
+	$("#one").click(function(){
+		$('#display').text($('#display').text() + 1);
+    });
+    $("#two").click(function(){
+		$('#display').text($('#display').text() + 2);
+    });
+    $("#three").click(function(){
+		$('#display').text($('#display').text() + 3);
+    });
+    $("#four").click(function(){
+		$('#display').text($('#display').text() + 4);
+    });
+    $("#five").click(function(){
+		$('#display').text($('#display').text() + 5);
+    });
+    $("#six").click(function(){
+		$('#display').text($('#display').text() + 6);
+    });
+    $("#seven").click(function(){
+		$('#display').text($('#display').text() + 7);
+    });
+    $("#eight").click(function(){
+		$('#display').text($('#display').text() + 8);
+    });
+    $("#nine").click(function(){
+		$('#display').text($('#display').text() + 9);
+    });
 
+	// Operations
+	$('#add').click(function() {
+  		$('#display').text($('#display').text() + '+');
+	});
+	$('#subtract').click(function() {
+  		$('#display').text($('#display').text() + '-');
+	});
+	$('#multiply').click(function() {
+  		$('#display').text($('#display').text() + '*');
+	});
+	$('#divide').click(function() {
+  		$('#display').text($('#display').text() + '/');
+	});
+	$('#decimal').click(function() {
+  		$('#display').text($('#display').text() + '.');
+	});
+	$('#equal').click(function() {
+  		$('#display').text(eval($('#display').text()));
+	});
 });
